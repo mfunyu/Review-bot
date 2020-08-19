e842ab2293e00712b791cfd58f9a6ceda00c76dc
mfunyu <mfunyu@student.42tokyo.jp>
2020-08-20 01:47:34 +0900

    modified req.txt

--- settings.py
+++ settings.py
@@ -11,5 +11,4 @@ from pathlib import Path  # python3 only
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-#DISCORD_TOKEN=os.getenv("FT_DISCORD_TOKEN")
-DISCORD_TOKEN=os.getenv("MY_DISCORD_TOKEN")
+DISCORD_TOKEN=os.getenv("FT_DISCORD_TOKEN")
