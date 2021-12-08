840940a896bf32b4d72dd1da921b73670a311a9b
mfunyu <mfunyu@student.42tokyo.jp>
2021-12-08 19:21:48 +0900

    feat: shalsh command done

--- reviewbot.py
+++ reviewbot.py
@@ -39,6 +39,38 @@ async def create_vc(ctx, project: str, time: str):
     reply = f'ボイスチャンネル {ch_name} を作成しました'
     await ctx.send(content=reply, hidden=True)
 
+@slash_client.slash(name="done")
+async def delete_vc(ctx, all: bool = False, current: bool = False):
+    for guild in bot.guilds:
+        if guild.name == constant.GUILD:
+            break
+    username = ctx.author.nick
+    if not username:
+        username = ctx.author.name
+    if func.status_in_vc(ctx.author, guild):
+        vc = ctx.author.voice
+        if username in vc.channel.name or current:
+            reply = f'{vc.channel.name} を削除しました。レビューお疲れ様でした :)'
+            await vc.channel.delete()
+            await ctx.send(content=reply, hidden=True)
+            return
+    reply = ''
+    cnt = 0
+    for channel in CATEGORY.channels:
+        if username in channel.name:
+            reply += f'\"{channel.name}\" '
+            cnt += 1
+    if not reply:
+        reply = f'{username}を含むチャンネルはありませんでした'
+    elif not all and cnt > 1:
+        reply += 'の複数のチャンネルが見つかりました'
+    else:
+        for channel in CATEGORY.channels:
+            if username in channel.name:
+                await channel.delete()
+        reply += 'を削除しました'
+    await ctx.send(content=reply, hidden=True)
+
 
 
 
