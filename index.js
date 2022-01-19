4991e5f5e3be5a939d00e5f8215fea3dd94c8022
mfunyu <mfunyu@student.42tokyo.jp>
2022-01-19 10:11:32 +0900

    update: use command modules

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('fs')
+const {Client, Intents} = require('discord.js');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
-client.once("ready", async () => {
-	const data = [{
-		name: "ping",
-		description: "Replies with Pong!",
-	},
-	{
-		name: "poooooong",
-		description: "Replies with Ping!",
-	}];
-	await client.application.commands.set(data, process.env.SERVER_ID);
-	console.log("Ready!");
+const commands = {};
+const commandFiles =
+    fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands[command.data.name] = command
+}
+
+client.once('ready', async () => {
+  const data = [];
+  for (const commandName in commands) {
+    data.push(commands[commandName].data)
+  }
+  await client.application.commands.set(data, '703838660080042015');
+  console.log('Ready!');
 });
 
-// reply
-client.on("interactionCreate", async (interaction) => {
-	if (!interaction.isCommand()) {
-		return;
-	}
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong！');
-	}
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+  const command = commands[interaction.commandName];
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    })
+  }
 });
 
-// ephemeral response
-client.on("interactionCreate", async (interaction) => {
-	if (!interaction.isCommand()) {
-		return;
-	}
-	if (interaction.commandName === 'poooooong') {
-		await interaction.reply({ content: 'Ping!', ephemeral: true });
-	}
-});
-
-
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.MY_DISCORD_TOKEN);
