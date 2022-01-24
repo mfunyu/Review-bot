9cb42f926a10a745dfb55bf7b4a32c855a4660fe
mfunyu <mfunyu@student.42tokyo.jp>
2022-01-24 11:07:27 +0900

    feat: delete all channels

--- commands/done.js
+++ commands/done.js
@@ -20,6 +20,34 @@ module.exports = {
 		]
 	},
 	async execute(interaction) {
-		await interaction.reply('channel deleted');
+		if (interaction.commandName == 'done') {
+			const guild = interaction.member.guild;
+
+			const userName = interaction.member.displayName;
+
+			const all = interaction.options.getBoolean('all');
+			const current = interaction.options.getBoolean('current');
+			const category = guild.channels.cache.find((channel) => channel.name === '📝 Project Review');
+
+			const channels = category.children;
+			const deleteChannels = []
+
+			if (all) {
+				channels.forEach(currentChannel => {
+					if (currentChannel.name.includes('/' + userName + '/')) {
+						deleteChannels.push(currentChannel);
+					}
+				});
+			}
+
+			let channelNames;
+
+			console.log(deleteChannels);
+			for (channel of deleteChannels) {
+				channelNames += channel.name + '\n';
+				await channel.delete();
+			}
+			await interaction.reply({ content: channelNames + 'を削除しました', ephemeral: true });
+		}
 	}
 }
