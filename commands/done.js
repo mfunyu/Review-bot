0159abe05c9158cea43d8dfe801da62609949174
mfunyu <mfunyu@student.42tokyo.jp>
2022-03-04 14:02:16 +0900

    refactor: turn into function

--- commands/done.js
+++ commands/done.js
@@ -33,71 +33,18 @@ module.exports = {
 
 			const channels = category.children;
 			const deleteChannels = [];
-			let ch;
 
 			switch (selection) {
 				case 'all':
-					let found = false;
-					channels.forEach(currentChannel => {
-						if (
-							currentChannel.name.includes('/' + userName + '/')
-						) {
-							deleteChannels.push(currentChannel);
-							found = true;
-						}
-					});
-					if (!found) {
-						await send.reply(
-							interaction,
-							send.msgs['NotFound'],
-							userName
-						);
+					if (deleteAll(interaction, channels, deleteChannels) == -1)
 						return;
-					}
 					break;
 				case 'current':
-					ch = getConnectingVoiceChannel(interaction);
-					if (!ch) {
-						await send.reply(interaction, send.msgs['NotInVC']);
+					if (deleteCurrent(interaction, deleteChannels) == -1)
 						return;
-					}
-					deleteChannels.push(ch);
 					break;
 				case 'choose':
-					var dict = ['zero', 'one', 'two', 'three', 'four'];
-					let msg_lists = '';
-					let index = 0;
-					const row = new Discord.MessageActionRow();
-					channels.forEach(currentChannel => {
-						if (
-							currentChannel.name.includes('/' + userName + '/')
-						) {
-							msg_lists += `\n:${dict[index]}:  \`${currentChannel.name}\``;
-							row.addComponents(
-								new Discord.MessageButton()
-									.setCustomId(currentChannel.name)
-									.setLabel(index.toString())
-									.setStyle('PRIMARY')
-							);
-							index++;
-						}
-					});
-					if (!msg_lists) {
-						await send.reply(
-							interaction,
-							send.msgs['NotFound'],
-							userName
-						);
-						return;
-					}
-					msg_lists += '\n';
-					await send.reply(
-						interaction,
-						send.msgs['Choose'],
-						msg_lists,
-						row
-					);
-					return;
+					deleteChoose();
 			}
 
 			const channelNames = formatChannelNames(deleteChannels);
@@ -106,6 +53,59 @@ module.exports = {
 		}
 	},
 };
+
+async function deleteChoose(interaction, deleteChannels) {
+	var dict = ['zero', 'one', 'two', 'three', 'four'];
+	let msg_lists = '';
+	let index = 0;
+	const row = new Discord.MessageActionRow();
+	channels.forEach(currentChannel => {
+		if (currentChannel.name.includes('/' + userName + '/')) {
+			msg_lists += `\n:${dict[index]}:  \`${currentChannel.name}\``;
+			row.addComponents(
+				new Discord.MessageButton()
+					.setCustomId(currentChannel.name)
+					.setLabel(index.toString())
+					.setStyle('PRIMARY')
+			);
+			index++;
+		}
+	});
+	if (!msg_lists) {
+		await send.reply(interaction, send.msgs['NotFound'], userName);
+		return;
+	}
+	msg_lists += '\n';
+	await send.reply(interaction, send.msgs['Choose'], msg_lists, row);
+	return;
+}
+
+async function deleteCurrent(interaction, deleteChannels) {
+	const ch = getConnectingVoiceChannel(interaction);
+	if (!ch) {
+		await send.reply(interaction, send.msgs['NotInVC']);
+		return -1;
+	}
+	deleteChannels.push(ch);
+	return 0;
+}
+
+async function deleteAll(interaction, channels, deleteChannels) {
+	const userName = interaction.member.displayName;
+	let found = false;
+
+	channels.forEach(currentChannel => {
+		if (currentChannel.name.includes('/' + userName + '/')) {
+			deleteChannels.push(currentChannel);
+			found = true;
+		}
+	});
+	if (!found) {
+		await send.reply(interaction, send.msgs['NotFound'], userName);
+		return -1;
+	}
+	return 0;
+}
 
 function formatChannelNames(deleteChannels) {
 	if (deleteChannels.length == 1) {
