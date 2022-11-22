de8ece1d14745d623cc87c833e0cc30bb6e1f2dd
mfunyu <mfunyu@student.42tokyo.jp>
2022-11-23 00:05:59 +0900

    update: /history embed format

--- commands/history.js
+++ commands/history.js
@@ -22,7 +22,7 @@ module.exports = {
 
 			const limit = 20;
 			const query =
-				`SELECT project, r.id, corrector, array_agg(corrected), to_char(begin_at, 'YYYY/MM/DD HH24:MI') as begin_at` +
+				`SELECT project, r.id, corrector, array_agg(corrected) as correcteds, to_char(begin_at, 'YYYY/MM/DD HH24:MI') as begin_at` +
 				' FROM reviews r' +
 				' RIGHT JOIN correcteds on r.id = review_id' +
 				' WHERE corrector = $1::text or corrected = $1::text' +
@@ -33,17 +33,41 @@ module.exports = {
 			const data = await pgClient.query(query, [displayName]);
 			console.dir(data.rows, { maxArrayLength: null });
 
-			let value = '';
+			let reviewer = '';
+			let reviewee = '';
 			data.rows.forEach(e => {
-				value += `・ ${e.begin_at} - ${e.project}\n`;
+				if (displayName == e.corrector) {
+					reviewer +=
+						`${e.begin_at}` +
+						` - \`${e.correcteds}\`` +
+						`｜*${e.project}*` +
+						'\n';
+				} else {
+					reviewee +=
+						`*${e.begin_at}*` +
+						` - \`${e.corrector}\`` +
+						`｜*${e.project}*` +
+						'\n';
+				}
 			});
-			if (!value) {
-				value = 'レビュー履歴はまだありません';
+			if (!reviewer) {
+				reviewer = 'レビュー履歴はまだありません';
+			}
+			if (!reviewee) {
+				reviewee = 'レビュー履歴はまだありません';
 			}
 			const field = [
 				{
 					name: user.username,
-					value,
+					value: `\`${data.rows.length}\`件のレビューを表示しています`,
+				},
+				{
+					name: '__reviewer__',
+					value: reviewer,
+				},
+				{
+					name: '__reviewee__',
+					value: reviewee,
 				},
 			];
 			await send.reply(interaction, send.msgs['History'], field);
