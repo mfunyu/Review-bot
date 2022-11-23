30ea05378bef47ff819e165133e8318adb832c7d
mfunyu <mfunyu@student.42tokyo.jp>
2022-11-23 16:00:19 +0900

    update: separate createTable funcs

--- db.js
+++ db.js
@@ -1,3 +1,29 @@
+exports.createTables = function (client) {
+	const promise1 = client.query(
+		'CREATE TABLE IF NOT EXISTS reviews ( \
+			id INTEGER PRIMARY KEY, \
+			corrector VARCHAR ( 25 ) NOT NULL, \
+			project VARCHAR ( 50 ) NOT NULL, \
+			begin_at TIMESTAMP WITH TIME ZONE NOT NULL \
+			)'
+	);
+
+	const promise2 = client.query(
+		'CREATE TABLE IF NOT EXISTS correcteds ( \
+			id VARCHAR ( 25 ) PRIMARY KEY, \
+			review_id INTEGER REFERENCES reviews (id) NOT NULL, \
+			corrected VARCHAR ( 25 ) NOT NULL \
+			)'
+	);
+
+	return Promise.all([promise1, promise2]);
+};
+
+exports.getDateToFetch = function (client) {
+	let query = 'SELECT begin_at FROM reviews ORDER BY begin_at DESC LIMIT 1';
+	return client.query(query);
+};
+
 exports.execInsert = function (data, client) {
 	let query =
 		'INSERT INTO reviews (id, corrector, project, begin_at) VALUES ';
